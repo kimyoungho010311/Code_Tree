dfcef390cb9dd8b3d563b8b3211c0b5f9038d145
Ocean_01 <96841065+kimyoungho010311@users.noreply.github.com>
2026-01-30 21:04:58 +0900

    Title: 숫자들의 배수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260130/숫자들의 배수/multiple-of-numbers.py
+++ 260130/숫자들의 배수/multiple-of-numbers.py
@@ -1,20 +1,19 @@
 N = int(input())
-mul_num = 2
-idx = 0
+
+arr = [N, 0, 0, 0, 0, 0, 0, 0, 0, 0] # size: 10
+answer = []
 cnt = 0
-arr = [N]
 
-while True:
-    tmp = arr[idx] * mul_num
-    mul_num += 1
-
-    arr.append(tmp)
-
-    if tmp % 5 == 0:
-        cnt += 1
-        if cnt == 2:
-            break
+for idx in range(1, 10):
+    arr[idx] = N * (idx+1)
 
 
 for elem in arr:
     print(elem, end=' ')
+    if arr[idx] % 5 == 0:
+        cnt += 1
+
+        if cnt == 2:
+            #print(arr[:idx+1])
+            arr = arr[:idx+1]
+            break
