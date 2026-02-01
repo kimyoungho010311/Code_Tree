50e42dd8307f52dea0aa8dabd1a45f28984d29db
Ocean_01 <96841065+kimyoungho010311@users.noreply.github.com>
2026-02-01 16:28:15 +0900

    Title: 전항의 두 배, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260201/전항의 두 배/twice-the-previous.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split())
+arr = [2, 3, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for idx in range(2, 10):
+    arr[idx] = arr[idx-1] + (arr[idx-2]) * 2
+
+
+for elem in arr:
+    print(elem, end=' ')
