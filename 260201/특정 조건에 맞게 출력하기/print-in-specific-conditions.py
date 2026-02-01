1ab3ee6f1cf6920f8a2ac66ad9809339fbce1fc8
Ocean_01 <96841065+kimyoungho010311@users.noreply.github.com>
2026-02-01 11:38:20 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260201/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
+++ 260201/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -1,17 +1,31 @@
-in_arr = list(map(int, input().split()))
-out_arr = []
+'''정수가 차례로 주어지다가 0이 주어지면 0을 제외하고 그때까지 주어진 정수를 차례로 출력하되
+그 수가 홀수이면 3을 더한 값을, 짝수이면 2로 나눈 몫을 출력하는 프로그램을 작성해보세요.'''
 
-# 배열을 입력받다 마지막 원소가 0임을 체크
-if in_arr[-1] == 0:
-    in_arr.pop()
+in_arr = list(map(int, input().split()))
+#
+# # 배열을 입력받다 마지막 원소가 0임을 체크
+out_arr = []
+# if in_arr[-1] == 0:
+#     in_arr.pop()
+#
+# for elem in in_arr:
+#     if elem % 2 != 0:
+#         elem += 3
+#         out_arr.append(elem)
+#
+#     elif elem % 2 == 0:
+#         out_arr.append(int(elem / 2))
+#
 
 for elem in in_arr:
-    if elem % 2 != 0:
-        elem += 3
-        out_arr.append(elem)
-
-    elif elem % 2 == 0:
-        out_arr.append(int(elem / 2))
+    if elem == 0:
+        break
+    else:
+        if elem % 2 != 0:
+            elem += 3
+            out_arr.append(elem)
+        elif elem % 2 == 0:
+            out_arr.append(int(elem / 2))
 
 
 for elem in out_arr:
