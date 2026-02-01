1a7e603f2769f4ad1c402cc310c6f5138d00559e
Ocean_01 <96841065+kimyoungho010311@users.noreply.github.com>
2026-02-01 11:34:09 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260201/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
+++ 260201/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -1,11 +1,15 @@
 in_arr = list(map(int, input().split()))
 out_arr = []
-in_arr.pop()
+
+# 배열을 입력받다 마지막 원소가 0임을 체크
+if in_arr[-1] == 0:
+    in_arr.pop()
 
 for elem in in_arr:
     if elem % 2 != 0:
         elem += 3
         out_arr.append(elem)
+
     elif elem % 2 == 0:
         out_arr.append(int(elem / 2))
 
