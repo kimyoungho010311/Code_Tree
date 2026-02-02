20bfc1bd526cf71fe389595e146a38181f3efb9b
Ocean_01 <96841065+kimyoungho010311@users.noreply.github.com>
2026-02-02 14:26:48 +0900

    Title: 점수대 파악하기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260202/점수대 파악하기/find-out-the-score-range.py
+++ 260202/점수대 파악하기/find-out-the-score-range.py
@@ -1,19 +1,15 @@
-input_arr = list(map(int, input().split()))
-no_zero = []
-check = [0 for _ in range(11)]
+# 배열에 주어진 수를 입력받아 저장합니다.
+arr = list(map(int, input().split()))
+count_arr = [0] * 11
 
-for elem in input_arr:
-    if elem != 0:
-        no_zero.append(elem)
-    elif elem == 0:
-        break
-
-for elem in no_zero:
-    if elem > 10:
-        check[elem // 10] += 1
-
-
-for idx in reversed(range(len(check))):
-    if idx * 10 == 0:
-        break
-    print(f"{idx * 10} - {check[idx]}")
+# 카운팅 배열을 통해 십의 자리수 각각의 빈도 저장, 0이 나오면 for문에서 빠져나오기
+for elem in arr:
+	if elem == 0:
+		break
+	if elem < 10:
+		continue
+	count_arr[elem // 10] += 1
+	
+# 100점대부터 10점대까지 나온 횟수를 출력
+for i in range(10, 0, -1):
+	print(f"{i}0 - {count_arr[i]}")
